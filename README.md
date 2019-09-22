c2d33f6dbee0edcd2290bbf88f542f51d30cd770
Dionathan de Córdova <dionathan_bass@hotmail.com>
2019-09-22 20:10:24 -0300

    documentação e alterações nas views

--- README.md
+++ README.md
@@ -8,7 +8,15 @@
 
 <p>Para iniciar a aplicação seguir os passos abaixo:</o>
 <p>1 - Conar este repositório</o>
-<p>2 - Após cloonar, no terminal o diretorio "sample_teste" que esta dentro do arquivo clonado</o>
-<p>3 - Apos isso, deve-se configurar a conexão ao banco de dados no arquivo ".env" que está no diretório raiz do projeto</o>
+<p>2 - Após clonar, no terminal acesse o diretorio "sample_teste" que esta dentro do arquivo clonado</o>
+<p>3 - Apos isso, deve-se configurar a conexão ao banco de dados como o nome de administrador e senha no arquivo ".env" que está no diretório raiz do projeto</o>
 <p>4 - Na linha 27: alterar as configuração conforme os dados de acesso a sua maquina está configurada "DATABASE_URL=mysql://username:password@127.0.0.1:3306/nomeDoBanco"</o>
-<p>5 - Dentro do diretorio do projeto através da linha de comando (CMD), como no exemplo "C:\Users\Usuario\Desktop\Sample-teste-vivitech\sample_teste> " <br>Executar agora os seguintes comandos para a criação do banco e tabela que estão vinculados a essa aplicação</o>
+<p>5 - Execute o comando "composer install", com este comando todas as dependencias que são necessárias no projeto são instaladas.</p>
+<p>6 - Ative seu servidor de banco de dados local ex: "XAMP / WAMP"</p>
+<p>7 - Execute o comando "php bin/console doctrine:database:create", isso fará que o banco de dados seja criado na sua maquina</p>
+<p>8 - Execute o comando "php bin/console doctrine:migrations:migrate", isso fará que a classe do ENTITY seja executada então são criadas as tabelas</p>
+<p>9 - Execute o comando "php -S localhost:8080 -t public", este comando executará o servidor interno do php</p>
+<p>10 - Acesse no navegor o endereço http://localhost:8080/</p>
+
+
+<p>Obrigado pela oportunidade de participar deste Teste. </p>
--- sample_teste/src/Controller/HomeController.php
+++ sample_teste/src/Controller/HomeController.php
@@ -17,13 +17,43 @@ class HomeController extends AbstractController {
         $posts = array(
             [
                 'id' => 1,
-                'nome' => 'dionathan',
-                'data' => '2019-02-02'
+                'nome' => 'Qualquer serviço de Repositorio / Versionamentoathan',
+                'done' => 'Github - https://github.com/dionathanCordova/Sample-teste-vivitech'
             ],
             [
                 'id' => 2,
-                'nome' => 'Namorada do dionathan',
-                'data' => '2019-02-02'
+                'nome' => 'Usar o Framework Symfony 2 ou Superior',
+                'done' => 'Aplicação utilizando Symfony 4.'
+            ],
+            [
+                'id' => 3,
+                'nome' => 'Deve conter uma ou mais Actions no Frontend e no BackEnd',
+                'done' => 'Aplicação possui 1 metodo controller e 1 action relacionados ao controller HomeController e 7 metodos e 3 actions relacionados ao controller UserControlller.'
+            ],
+            [
+                'id' => 4,
+                'nome' => 'Use Doctrine pelo Symfony eles se dão muito bem, chame o amigo Mysql para participar.',
+                'done' => 'Desde a criação do banco de dados, até o Crud que a aplicação utiliza, está sendo feito com o Doctrine e Mysql.'
+            ],
+            [
+                'id' => 5,
+                'nome' => 'Usar Twig ou algo similar que trabalhe com template enginer.',
+                'done' => 'Embora tenha sido usado somente o básico, a aplicação utiliza Twig em todas as Views.'
+            ],
+            [
+                'id' => 6,
+                'nome' => 'Saber alguma coisa de micro serviços: Docker, Lambda, Docker-compose e afins (Mostre-nos o que voce sabe).',
+                'done' => 'Sei somente o básico da teoria sobre Docker, mas tenho uma meta de logo começar a utilizar esta tecnologia.'
+            ],
+            [
+                'id' => 7,
+                'nome' => 'CSS/JS framework como Bootstrap/UIKit.',
+                'done' => 'O Navbar, tabela de usuários cadastrados e os formulários foram feitos com bootstrap.'
+            ],
+            [
+                'id' => 8,
+                'nome' => 'PHPUnit Test ou Selenium.',
+                'done' => 'Me impuz a meta de entregar o maximo possível até a data 23/09/2019. então não foi possível estudar e agregar nada de TDD na aplicação.'
             ]
         );
 
@@ -32,23 +62,5 @@ class HomeController extends AbstractController {
             'teste' => 'testando como passar o parametro',
             'posts' => $posts
         ]);
-    }
-
-    /**
-    * @Route("/hello", name="hello")
-    */
-    public function hello() {
-        return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
-        // 'name' => $name
-        ]);
-    }
-
-  
-    /**
-     * @Route("/teste", name="teste")
-     */
-    public function teste(){
-        return new Response('teste');
     }
 }
